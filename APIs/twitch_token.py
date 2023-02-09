2f70a7bd7530ecaa33ccd822b27a2e362a9ba570
sudulge <hhs0888@naver.com>
2023-02-10 01:48:12 +0900

    토큰 파일 gitignore 추가

--- APIs/twitch_token.py
+++ APIs/twitch_token.py
@@ -5,19 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 사용자 액세스 토큰  크롬 주소창에 직접 입력. # chat read, chat edit 스코프
-# url = https://id.twitch.tv/oauth2/authorize
-#       ?response_type=token
-#       &client_id=<your client id>
-#       &redirect_uri=http://localhost:5000
-#       &scope=chat%3Aread+chat%3Aedit
+# url = f'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={os.getenv("twitch_client_id")}&redirect_uri=http://localhost:5000&scope=chat%3Aread+chat%3Aedit'
 
 # 앱 액세스 토큰 발급 .post params
-# url = 'https://id.twitch.tv/oauth2/token'
-# params = {
-#     'client_id' : os.getenv("twitch_client_id"),
-#     'client_secret' : os.getenv("twitch_client_secret"),
-#     'grant_type' : 'client_credentials',
-# }
+url = 'https://id.twitch.tv/oauth2/token'
+params = {
+    'client_id' : os.getenv("twitch_client_id"),
+    'client_secret' : os.getenv("twitch_client_secret"),
+    'grant_type' : 'client_credentials',
+}
 
 # 토큰 제거 .post data
 # url = 'https://id.twitch.tv/oauth2/revoke'
@@ -28,12 +24,13 @@ load_dotenv()
 
 
 # 토큰 유효성 검사 .get headers
-url = 'https://id.twitch.tv/oauth2/validate'
-headers = {
-    'Authorization' : os.getenv("twitch_app_access_token"),
-}
+# url = 'https://id.twitch.tv/oauth2/validate'
+# headers = {
+#     'Authorization' : os.getenv("twitch_user_access_token"),
+# }
 
-response = requests.get(url, headers=headers)
+response = requests.post(url, params=params)
 contents = response.text
 
 print(contents)
+
